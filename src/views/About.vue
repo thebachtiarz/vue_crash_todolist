a552548aba8b5aedda8e8c5ae777457fd2274e4e
TheBachtiarz <bachtiarptibinlppnri2417@gmail.com>
2019-11-02 15:23:38 +0700

    todo list router link finish

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>Todo List Apps v1.0.0 - Vue CLI v4 - created by thebachtiarz.</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.about {
+  text-align: center;
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+}
+</style>
